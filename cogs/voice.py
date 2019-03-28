c707416b0059ed7a74db36122bfba9a4948e1b85
SamSanai <38131935+SamSanai@users.noreply.github.com>
2019-03-28 14:40:10 +0000

    Fixed cog errors (discord.py Update)

--- cogs/voice.py
+++ cogs/voice.py
@@ -17,7 +17,7 @@ class voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         conn = sqlite3.connect('voice.db')
         c = conn.cursor()
