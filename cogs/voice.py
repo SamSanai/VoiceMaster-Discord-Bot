3dcdf2ce8df83263e0cd6a007b8dae5184e7be4d
Zach Thomas <zwthomas13@gmail.com>
2020-05-15 11:06:52 -0500

    Now properly name created private channel, and createe waiting channel, still need to clean waiting channel

--- cogs/voice.py
+++ cogs/voice.py
@@ -162,17 +162,19 @@ class voice(commands.Cog):
         category = self.bot.get_channel(requestedInfo[1])
         createdChannel = await member.guild.create_voice_channel("Public",category=category)
         await member.move_to(createdChannel)
-        db.execute("INSERT INTO channels VALUES (?, ?)", (createdChannel.id,requestedInfo[0]))
+        db.execute("INSERT INTO channels VALUES (?, ?, ?)", (createdChannel.id, requestedInfo[0], None))
 
     async def createPrivateChannel(self, member, db, requestedInfo):
         category = self.bot.get_channel(requestedInfo[1])
         guild = self.bot.get_guild(requestedInfo[0])
         everyone = [role for role in guild.roles if role.name == "@everyone"][0]
-        createdChannel = await member.guild.create_voice_channel("Private",category=category)
+        createdChannel = await member.guild.create_voice_channel(member.display_name,category=category)
+        waitingChannel = await member.guild.create_voice_channel(member.display_name + "'s Waiting Room", category=category)
         await createdChannel.set_permissions(everyone,connect = False)
         await createdChannel.set_permissions(member, connect = True, move_members = True)
+        await waitingChannel.set_permissions(member, move_members = True)
         await member.move_to(createdChannel)
-        db.execute("INSERT INTO channels VALUES (?, ?)", (createdChannel.id,requestedInfo[0]))
+        db.execute("INSERT INTO channels VALUES (?, ?, ?)", (createdChannel.id, requestedInfo[0], waitingChannel.id))
 
     # https://discordapp.com/developers/docs/topics/permissions
     def isAdmin(self, ctx):
