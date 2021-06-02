856790115cf3fd801f5b6ef1ab659f569e5c3e36
Kye <71964154+kyelmao@users.noreply.github.com>
2021-06-02 09:29:07 +0200

    typo lol

--- voicecreate.py
+++ voicecreate.py
@@ -4,6 +4,8 @@ import traceback
 import sys
 from discord.ext.commands import AutoShardedBot as asb
 
+started = False
+
 class VoiceMaster(asb):
     def __init__(self):
         super().__init__(
@@ -13,21 +15,24 @@ class VoiceMaster(asb):
             intents=discord.Intents.all() # remove this if you dont want to enable all intents
         )
         
-        self.cog_blaclist = [
+        self.cog_blacklist = [
             "__init__.py",
             "functions.py"
         ]
         self.remove_command("help")
         self.token = "TOKEN HERE" # please consider using a secrets file or a token file with a .gitignore
-        
-        print("Loading cogs:")
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py") and filename not in self.cog_blacklist:
-                try:
-                    self.load_extension(f"cogs.{filename[:-3]}")
-                    print(f"    Loaded '{filename}'")
-                except Exception as e:
-                    print(str(e))
+    
+    def load_cogs(self):
+        if not started:
+            print("Loading cogs:")
+            for filename in os.listdir("./cogs"):
+                if filename.endswith(".py") and filename not in self.cog_blacklist:
+                    try:
+                        self.load_extension(f"cogs.{filename[:-3]}")
+                        print(f"    Loaded '{filename}'")
+                    except Exception as e:
+                        print(str(e))
+            started = not started
 
     async def on_connect(self):
         print("Connected")
