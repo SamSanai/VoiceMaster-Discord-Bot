1a9fdacfe3c3ebf7ab752ef436ccba4a1efd374d
2T-ae <66900741+2T-ae@users.noreply.github.com>
2022-01-29 13:16:53 +0900

    Update voice.py

--- cogs/voice.py
+++ cogs/voice.py
@@ -97,7 +97,7 @@ class voice(commands.Cog):
         c = conn.cursor()
         guildID = ctx.guild.id
         id = ctx.author.id
-        if ctx.author.id == ctx.guild.owner.id or ctx.author.id == 151028268856770560:
+        if ctx.author.id == ctx.guild.owner_id or ctx.author.id == 151028268856770560:
             def check(m):
                 return m.author.id == ctx.author.id
             await ctx.channel.send("**You have 60 seconds to answer each question!**")
